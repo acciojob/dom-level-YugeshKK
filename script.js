57d1f3851cddc5e55263184fe9d1310f79642766
YugeshKK <95625392+YugeshKK@users.noreply.github.com>
2023-09-29 13:46:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+const ele= document.getElementById('level');
+
+function getaparent(ele) {
+	let count=0;
+
+	while(ele){
+		count++;
+		ele=ele.parentElement;
+	}
+
+	return count;
+}
+
+const count= getaparent(ele);
+
+window.alert(count);
